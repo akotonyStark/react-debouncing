cda25933de20e0637472aaa413279b694bf11a07
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-02-21 15:59:58 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <input
+          type='text'
+          style={{ width: 400, height: 30, padding: 10 }}
+          placeholder='search here'
+        />
+
+        <div style={styles.results}>Res</div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+const styles = {
+  results: {
+    height: 400,
+    width: 400,
+    background: 'white',
+    marginTop: 50,
+  },
+}
